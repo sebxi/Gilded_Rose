d7f86ad3c7c6f645f6f402571a189dc2ce10bbf4
Sebastian <anderseka.sebastian@yahoo.com>
2025-07-01 17:09:30 +0200

    T1: SellIn Value

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -1,9 +1,8 @@
 describe("Gilded Rose", function() {
 
-  it("should foo", function() {
-    items = [ new Item("foo", 0, 0) ];
-    update_quality();
-    expect(items[0].name).toEqual("fixme");
+  it("should store the correct sell_in value when item is created", function() {
+    const item = new Item("Test Item", 10, 20);
+    expect(item.sell_in).toEqual(10);
   });
 
 });
