9f84aae09e90e4976079381cb0a03e288de5343b
Sebastian <anderseka.sebastian@yahoo.com>
2025-07-01 17:12:26 +0200

    T2: Quality Value

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -1,8 +1,15 @@
 describe("Gilded Rose", function() {
 
+  //T1 SellIn Value
   it("should store the correct sell_in value when item is created", function() {
     const item = new Item("Test Item", 10, 20);
     expect(item.sell_in).toEqual(10);
+  });
+
+  //T2 Quality Value
+  it("should store the correct quality value when item is created", function() {
+    const item = new Item("Test Item", 5, 30);
+    expect(item.quality).toEqual(30);
   });
 
 });
